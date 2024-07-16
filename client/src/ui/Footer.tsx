d59983080c91495dff5deb8304c858318f81694c
arafatGithubCode <md489288@gmail.com>
2024-07-16 13:12:50 +0600

    complete footer

--- client/src/ui/Footer.tsx
+++ client/src/ui/Footer.tsx
@@ -1,5 +1,17 @@
+import Container from "./Container";
+import { payment } from "../assets";
+import FooterTop from "./FooterTop";
+
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <div className="mt-10">
+      <FooterTop />
+      <Container className="flex flex-col md:flex-row items-center gap-4 justify-between">
+        <p>@2024 E-commerce solutions. All rights reserved.</p>
+        <img src={payment} alt="payment-img" className="object-cover" />
+      </Container>
+    </div>
+  );
 };
 
 export default Footer;
